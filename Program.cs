e6256835cf34ea2fac604bf331028706ab2cd091
Nimrod Maina <nimzy.maina@gmail.com>
2019-05-20 16:01:07 +0300

    Cancel thread from multiple cancel tokens

--- Program.cs
+++ Program.cs
@@ -8,44 +8,28 @@ namespace threads
     {
         static void Main(string[] args)
         {
-            // 
-            // declear cancellation token source
-            var cts = new CancellationTokenSource();
-            // get cancellation token
-            var token = cts.Token;
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
 
-            // declare task to run in a different thread
-            var t = new Task(() => {
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+                planned.Token, preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(()=>{
                 int i = 0;
-                while(true)
-                {
-                    if(token.IsCancellationRequested){
-                        // Show thread Id
-                        Console.WriteLine($"The task was running on thead ID ===> {Task.CurrentId}");
-                        throw new OperationCanceledException();
-                        // or ---> break;
-                    }
-                    // or instead of if statement call below method (Recommended way)
-                    // token.ThrowIfCancellationRequested();
-                    else
-                        Console.Write($"{i++} \t");
+                while(true){
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++} \t");
+                    Thread.Sleep(1000);
                 }
-            },token);
-            t.Start();
-
-            // Use this if you want to cleanup after the task has finished executing completely
-            Task.Factory.StartNew(() => {
-                // Blocking call
-                token.WaitHandle.WaitOne();
-                Console.WriteLine("Wait handle has been release");
-            });
+            },paranoid.Token);
 
             Console.ReadKey();
-            cts.Cancel();
+            planned.Cancel();
 
-            Console.WriteLine($"Main program has ended on  thread {Task.CurrentId}");
-            Console.WriteLine($"Task status {t.Status.ToString()}");
+            Console.WriteLine("Program ended");
             Console.ReadKey();
+            
         }
     }
 }
